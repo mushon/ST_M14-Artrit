211386d3ddd0e718ffa13585964b59fddde8ccf9
Mushon Zer-Aviv <mushon@shual.com>
2022-07-05 19:04:07 +0300

    fixed Embed

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -1,6 +1,6 @@
 export const AIRTABLE_BASE = 'appowVryh3gfUSkbN';
 export const AIRTABLE_API_KEY = 'keysbVnYZk16tqIT0';
-export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/shrQYzUght6QWpzs2';
+export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/embed/shrQYzUght6QWpzs2';
 
 export const MAPBOX_STYLE = 'mapbox://styles/atlasmedliq/ckiodibg82x1k17tcof8e9pmd';
 export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXRsYXNtZWRsaXEiLCJhIjoiY2tpbXgzNW5qMHhhcjJ5cGtydHpkNnJqYyJ9.vfietFuvTA8S1vaGlm3CUQ';
