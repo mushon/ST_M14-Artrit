299cd53b131b14be94a1f1e9b15ef9619eaded6d
Mushon Zer-Aviv <mushon@shual.com>
2022-10-16 17:09:17 +0300

    Update CONFIGURATION.ts

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -3,4 +3,10 @@ export const AIRTABLE_API_KEY = 'keysbVnYZk16tqIT0';
 export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/embed/shrQYzUght6QWpzs2';
 
 export const MAPBOX_STYLE = 'mapbox://styles/atlasmedliq/cl59k9bun000y14rzt3yy5ljt';
+export const MAPBOX_BASE_STYLE = 'mapbox://styles/atlasmedliq/cl59k9bun000y14rzt3yy5ljt';
 export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXRsYXNtZWRsaXEiLCJhIjoiY2tpbXgzNW5qMHhhcjJ5cGtydHpkNnJqYyJ9.vfietFuvTA8S1vaGlm3CUQ';
+
+export const HORIZONTAL_LAYOUT = true; // enable for horizontal layout
+
+export const PRIMARY_COLOR = '#CE0031';
+export const SECONDARY_COLOR = '#6E0023';
